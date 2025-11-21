c6ee7642ea7c6308a1550a1a8c70efb038f298a8
lateef123442 <umuyetuahmed@gmail.com>
2025-11-22 00:26:01 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound";
 import TakeAssessment from "./components/assessment/TakeAssessment";
 import AssessmentReview from "./components/assessment/AssessmentReview";
+import takemock from "./components/assessment/takemock";
 
 const queryClient = new QueryClient();
 
@@ -24,6 +25,7 @@ const App = () => (
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/take-assessment/:attemptId" element={<TakeAssessment />} />
           <Route path="/assessment-review/:attemptId" element={<AssessmentReview />} />
+          <Route path="/takemock/:attemptId" element={<takemock />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
