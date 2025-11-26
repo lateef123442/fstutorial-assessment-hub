960c47648024099f7589d149b85e130cc54f29bb
lateef123442 <umuyetuahmed@gmail.com>
2025-11-26 03:48:07 +0100

    Update StudentDashboard.tsx

--- src/components/dashboard/StudentDashboard.tsx
+++ src/components/dashboard/StudentDashboard.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { supabase } from "@/integrations/supabase/client";
 import { toast } from "sonner";
 import AvailableAssessments from "./student/AvailableAssessments";
-import Mock from "./student/Mock"; 
+import MockExam from "./student/MockExam"; 
 import MyResults from "./student/MyResults";
 
 
